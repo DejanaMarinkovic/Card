0bf0f1b95c193dc418318d4e5e4f877b0032b364
DejanaMarinkovic <118027271+DejanaMarinkovic@users.noreply.github.com>
2023-02-26 20:17:51 +0100

    Update Card.py

--- Card.py
+++ Card.py
@@ -7,38 +7,38 @@ class Card:
 
     def pay(self, amount):
         if self.balance < amount:
-            print ( "Insufficient funds on the card" )
+            print("Insufficient funds on the card")
         else:
             self.balance -= amount
-            print ( f"Successful payment in the amount of {amount} dinars. New balance: {self.balance} dinars." )
+            print(f"Successful payment in the amount of {amount} dinars. New balance: {self.balance} dinars.")
 
 
 class Visa ( Card ):
     def __init__(self, number, balance):
-        super ().__init__ ( number, balance )
+        super ().__init__(number, balance)
 
     def pay(self, amount):
         tax = amount * 0.05
         total_amount = amount + tax
         if self.balance < total_amount:
-            print ( "Insufficient funds on the card" )
+            print("Insufficient funds on the card")
         else:
             self.balance -= total_amount
-            print (f"Successful payment in the amount of {amount} dinars with tax of {tax} dinars. New balance: {self.balance} dinars." )
+            print(f"Successful payment in the amount of {amount} dinars with tax of {tax} dinars. New balance: {self.balance} dinars.")
 
 
 class Master ( Card ):
     def __init__(self, number, balance):
-        super ().__init__ ( number, balance )
+        super ().__init__(number, balance)
 
     def pay(self, amount):
         tax = amount * 0.08
         total_amount = amount + tax
         if self.balance < total_amount:
-            print ( "Insufficient funds on the card" )
+            print("Insufficient funds on the card")
         else:
             self.balance -= total_amount
-            print (f"Successful payment in the amount of {amount} dinars with tax of {tax} dinars. New balance: {self.balance} dinars." )
+            print(f"Successful payment in the amount of {amount} dinars with tax of {tax} dinars. New balance: {self.balance} dinars.")
 
 
 visa_card = Visa(123456, 1000)
